896044895420257ed5eb5c8efe93ec953b8d792d
Shelomentsev Dmitriy <shelomentsev@protonmail.ch>
2016-06-25 23:02:12 +0300

    LaundryTags names filed

--- frontend/other/index.js
+++ frontend/other/index.js
@@ -2,117 +2,117 @@ const LaundryTags = [
 	{
 		img:"/assets/icons/wh-washing-30deg.svg",
 		description:"Стирка в тёплой воде (Температура до 30 С)",
-		name:''
+		name:'washing-30deg'
 	},
 	{
 		img:"/assets/icons/wh-washing-40deg.svg",
 		description:"Стирка белья в теплой воде с нейтральными моющими средствами и стирка цветного белья (Температура до 40 С)",
-		name:''
+		name:'washing-40deg'
 	},
 	{
 		img:"/assets/icons/wh-washing-50deg.svg",
 		description:"Стирка цветного белья (Температура до 50 С)",
-		name:''
+		name:'washing-50deg'
 	},
 	{
 		img:"/assets/icons/wh-washing-60deg.svg",
 		description:"Стирка цветного белья (Температура до 60 С). Изделия не стойкое к кипячению, например, тонкое белье из хлопка или полиэстровых тканей.",
-		name:''
+		name:'washing-60deg'
 	},
 	{
 		img:"/assets/icons/wh-washing-70deg.svg",
 		description:"Стирка цветного белья (Температура до 70 С)",
-		name:''
+		name:'washing-70deg'
 	},
 	{
 		img:"/assets/icons/wh-washing-95deg.svg",
 		description:"Стирка с кипячением. Изделия из хлопка и льна, белое или цветное стойкое к кипячению.",
-		name:''
+		name:'washing-95deg'
 	},
 	{
 		img:"/assets/icons/wh-washing.svg",
 		description:"Нормальная стирка",
-		name:''
+		name:'washing'
 	},
 	{
 		img:"/assets/icons/wh-washing-permanent-press.svg",
 		description: "Щадящая стирка. Точно выдерживать температуру воды, не подвергать сильной механической обработке, при отжиме - медленный режим центрифуги",
-		name:''
+		name:'washing-permanent-press'
 	},
 	{
 		img:"/assets/icons/wh-washing-extra-care.svg",
 		description:"Деликатная стирка. Большое количество воды, минимальная механическая обработка, быстрое полоскание.",
-		name:''
+		name:'washing-extra-care'
 	},
 	{
 		img:"/assets/icons/wh-washing-hand.svg",
 		description: "Только ручная стирка, не подлежит стирке в стиральной машине. Не тереть, не отжимать. Максимальная температура - 40 С.",
-		name: ''
+		name: 'wh-washing-hand'
 	},
 	{
 		img:"/assets/icons/wh-washing-not-allowed.svg",
 		description:"Стирка запрещена",
-		name:''
+		name:'washing-not-allowed'
 	},
 	{
 		img:"/assets/icons/wh-wringing-not-allowed.svg",
 		description:"Не отжимать, не выкручивать",
-		name:''
+		name:'wringing-not-allowed'
 	},
 	{
 		img:"/assets/icons/wh-bleaching.svg",
 		description:"Можно отбеливать",
-		name:''
+		name:'bleaching'
 	},
 	{
 		img:"/assets/icons/wh-bleaching-not-allowed.svg",
 		description:"Нельзя отбеливать. При стирке не использовать средства, содержащие отбеливатели (хлор)",
-		name:''
+		name:'bleaching-not-allowed'
 	},
 	{
 		img:"/assets/icons/wh-bleaching-non-chlorine.svg",
 		description:"Можно отбеливать, но только без хлора",
-		name:''
+		name:'bleaching-non-chlorine'
 	},
 	{
 		img:"/assets/icons/wh-drycleaning.svg",
 		description:"Cухая чистка",
-		name:''
+		name:'drycleaning'
 	},
 	{
 		img:"/assets/icons/wh-drycleaning-not-allowed.svg",
 		description:"Химчистка запрещена",
-		name:''
+		name:'drycleaning-not-allowed'
 	},
 	{
 		img:"/assets/icons/wh-drying-tumble.svg",
 		description:"Можно отжимать и сушить в стиральной машине",
-		name:''
+		name:'drying-tumble'
 	},
 	{
 		img:"/assets/icons/wh-drying-tumble-not-allowed.svg",
 		description:"Нельзя выжимать и сушить в стиральной машине",
-		name:''
+		name:'drying-tumble-not-allowed'
 	},
 	{
 		img:"/assets/icons/wh-drying-not-allowed.svg",
 		description:"Не сушить",
-		name:''
+		name:"drying-not-allowed"
 	},
 	{
 		img:"/assets/icons/wh-drying-tumble-low-heat.svg",
 		description:"Сушить при низкой температуре (щадящая сушка)",
-		name:''
+		name:"drying-tumble-low-heat"
 	},
 	{
 		img:"/assets/icons/wh-drying-tumble-medium-heat.svg",
 		description:"Сушить при средней температуре (нормальная сушка)",
-		name:''
+		name:"drying-tumble-medium-heat"
 	},
 	{
 		img:"/assets/icons/wh-drying-tumble-high-heat.svg",
 		description:"Сушить при высокой температуре",
-		name:''
+		name:"drying-tumble-high-heat"
 	}
 ];
 
