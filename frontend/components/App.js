cfb99aa533f57e5bf4a77b395df894a304ce21a2
hypsteria <nep.lubov.alex@gmail.com>
2016-06-25 19:53:17 +0300

    adding Alert component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Menu from './Menu';
 import Content from './Content';
 import { Row, Col } from 'react-bootstrap';
+import AlertDismissable from './AlertDismissable';
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
 		<Row>
 			<Col xs={12} md={12} >
 				<Menu />
+				<AlertDismissable message='some text' />
 				<Content />
 			</Col>
 		</Row>
