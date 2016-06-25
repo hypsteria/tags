e2f3e81366b4e26c1530f6c01a952ca810c990be
hypsteria <nep.lubov.alex@gmail.com>
2016-06-26 01:24:58 +0300

    new clothes

--- frontend/other/DummyBase.js
+++ frontend/other/DummyBase.js
@@ -9,12 +9,132 @@ const DummyBase = {
 			id: '1111212423432'
 		},
 		{
-			img: '/assets/img/tshirt.jpg',
-			name: 'Вторая футболка',
+			img: '/assets/img/clo-1.jpeg',
+			name: 'Синее платье',
 			selected: false,
 			hide: false,
-			tags: ['washing-50deg', 'bleaching-not-allowed'],
-			id: '235134543'
+			tags: ['washing-30deg', 'bleaching-not-allowed'],
+			id: '23513324543'
+		},
+		{
+			img: '/assets/img/clo-2.jpeg',
+			name: 'Цветная рубашка',
+			selected: false,
+			hide: false,
+			tags: ['washing-30deg', 'bleaching-not-allowed'],
+			id: '321423'
+		},
+		{
+			img: '/assets/img/clo-3.jpeg',
+			name: 'Светлые джинсы',
+			selected: false,
+			hide: false,
+			tags: ['washing-60deg', 'bleaching-not-allowed'],
+			id: '6453562'
+		},
+		{
+			img: '/assets/img/clo-4.jpeg',
+			name: 'Уличная толстовка',
+			selected: false,
+			hide: false,
+			tags: ['washing-60deg', 'bleaching-not-allowed'],
+			id: '2315'
+		},
+		{
+			img: '/assets/img/clo-5.jpeg',
+			name: 'Вечерний наряд',
+			selected: false,
+			hide: false,
+			tags: ['washing-30deg', 'bleaching-not-allowed'],
+			id: '2143'
+		},
+		{
+			img: '/assets/img/clo-7.jpeg',
+			name: 'Летнее платье',
+			selected: false,
+			hide: false,
+			tags: ['washing-30deg', 'bleaching-not-allowed'],
+			id: '324544'
+		},
+		{
+			img: '/assets/img/clo-8.jpeg',
+			name: 'Летний костюм',
+			selected: false,
+			hide: false,
+			tags: ['washing-30deg', 'bleaching-not-allowed'],
+			id: '553'
+		},
+		{
+			img: '/assets/img/clo-9.jpeg',
+			name: 'Тёмно-синяя футболка',
+			selected: false,
+			hide: false,
+			tags: ['washing-30deg', 'bleaching-not-allowed'],
+			id: '76856'
+		},
+		{
+			img: '/assets/img/clo-10.jpeg',
+			name: 'Бирюзовая рубашка',
+			selected: false,
+			hide: false,
+			tags: ['washing-30deg', 'bleaching-not-allowed'],
+			id: '236456'
+		},
+		{
+			img: '/assets/img/clo-11.jpeg',
+			name: 'Пляжные шорты',
+			selected: false,
+			hide: false,
+			tags: ['washing-30deg', 'bleaching-not-allowed', 'drying-tumble-not-allowed'],
+			id: '1234234'
+		},
+		{
+			img: '/assets/img/clo-12.jpg',
+			name: 'Серая футболка',
+			selected: false,
+			hide: false,
+			tags: ['washing-30deg', 'bleaching-not-allowed'],
+			id: '5234'
+		},
+		{
+			img: '/assets/img/clo-13.jpg',
+			name: 'Черные шорты',
+			selected: false,
+			hide: false,
+			tags: ['washing-30deg', 'bleaching-not-allowed'],
+			id: '34514'
+		},
+		{
+			img: '/assets/img/clo-14.jpg',
+			name: 'Пляжное платье',
+			selected: false,
+			hide: false,
+			tags: ['wh-washing-hand', 'bleaching-not-allowed', 'drying-tumble-not-allowed'],
+			id: '745674567'
+		},
+		{
+			img: '/assets/img/clo-15.jpg',
+			name: 'Чёрное платье',
+			selected: false,
+			hide: false,
+			tags: ['wh-washing-hand', 'bleaching-not-allowed', 'drying-tumble-not-allowed'],
+			id: '1344431'
+		},
+		{
+			img: '/assets/img/clo-16.jpg',
+			name: 'Розовое платье',
+			selected: false,
+			hide: false,
+			tags: ['washing-95deg', 'bleaching-not-allowed'],
+			id: '345345'
+		},
+		{
+			img: '/assets/img/clo-17.jpg',
+			name: 'Джинсы',
+			selected: false,
+			hide: false,
+			tags: ['washing-60deg', 'bleaching-not-allowed'],
+			id: '58467547634'
 		}
 	]
 }
