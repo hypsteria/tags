578daf6b840e694f7aeb126d82c6f482590c50be
Shelomentsev Dmitriy <shelomentsev@protonmail.ch>
2016-06-26 10:16:59 +0300

    AlertDismissable

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Menu from './Menu';
 import Content from './Content';
 import { Row, Col } from 'react-bootstrap';
-import AlertDismissable from './AlertDismissable';
 
 const App = ({children}) => {
 	return (
 		<div>
 			<Menu />
 			<div className="container">
-				<AlertDismissable />
 				<Content>{children}</Content>
 			</div>
 		</div>
--- frontend/components/ClotheList.js
+++ frontend/components/ClotheList.js
@@ -3,20 +3,25 @@ import uuid from 'uuid-v4'
 import { connect } from 'react-redux'
 import { addToLaundry, removeFromLaundry } from '../actions'
 import ClotheItem from './ClotheItem'
+import AlertDismissable from './AlertDismissable'
 
 const ClotheList = ({items, dispatch}) => {
-	return <ul className="clotheList">{
-		items.map(({id, img, name, selected, hide} = item, i) => {
-			return <ClotheItem buttonAdd={ () => (dispatch(addToLaundry(id))) }
-							   buttonRemove={ () => (dispatch(removeFromLaundry(id))) }
-							   id={id}
-							   img={img}
-							   name={name}
-							   selected={selected}
-							   hide={hide}
-							   key={uuid()}/>;
-		})
-	}</ul>;
+	return (
+		<div>
+			<AlertDismissable /> 
+			<ul className="clotheList">{
+				items.map(({id, img, name, selected, hide} = item, i) => {
+					return <ClotheItem buttonAdd={ () => (dispatch(addToLaundry(id))) }
+									   buttonRemove={ () => (dispatch(removeFromLaundry(id))) }
+									   id={id}
+									   img={img}
+									   name={name}
+									   selected={selected}
+									   hide={hide}
+									   key={uuid()}/>;
+				})
+			}</ul>
+		</div>);
 };
 
 const mapStateToProps = (state, ownProps) => {
